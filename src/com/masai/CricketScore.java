8f2ff0bb3efecef45f5f77e34fe2eec4a33516e4
chaitali amale <chaitaliamale09@gmail.com>
2022-05-17 22:37:36 +0530

    Assignment

---
+++ src/com/masai/CricketScore.java
@@ -0,0 +1,11 @@
+package com.masai;
+
+public class CricketScore {
+	static void cricketScore(int r1,int r2,int r3,int r4,int r6) {
+		int result=(r1*1+r2*2+r3*3+r4*4+r6*6);
+		System.out.println("Total score by Batsman: "+result);
+	}
+	public static void main(String args[]) {
+		cricketScore(2,3,4,5,6);
+	}
+}
---
+++ src/com/masai/SumOfNaturalNumber.java
@@ -0,0 +1,11 @@
+package com.masai;
+
+public class SumOfNaturalNumber {
+	static void sumOfNaturalNumber(int num){
+		int sum=(num*(num+1))/2;
+		System.out.println(sum);
+	}
+		public static void main(String args[]){
+			sumOfNaturalNumber(34);
+		}
+}
